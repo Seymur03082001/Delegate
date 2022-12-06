59cbca918f7dd097a0a5056901dae8b6a89dc515
Seymur03082001 <116655341+Seymur03082001@users.noreply.github.com>
2022-12-06 05:07:30 +0400

    first commit

---
+++ Delegate/Models/People.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate.Models
+{
+    internal class People<T>
+    {
+        T[] arr;
+        private string _name;
+        private string _surname;
+        private int _age;
+
+        
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+        
+        public string Surname        
+        {
+            get { return _surname; }
+            set { _surname = value; }
+        }
+
+        
+        public int Age
+        {
+            get { return _age; }
+            set { _age = value; }
+        }
+        public People(T[] arr, string name, string surname, int age)
+        {
+            this.arr = arr;
+            Name = name;
+            Surname = surname;
+            Age = age;
+        }
+
+        public void FindAll(string name,string surname,int age) 
+        { 
+            foreach (var item in arr) 
+            {
+                if (item)
+                {
+                    age++;
+                }
+            }
+        }
+    }
+}
---
+++ Delegate/Program.cs
@@ -0,0 +1,14 @@
+﻿using Delegate.Models;
+
+namespace Delegate
+{
+    internal class Program
+    {
+        delegate void InformationPeople(People people);
+        static void Main(string[] args)
+        {
+            List<int> people = new List<int>();
+            
+        }
+    }
+}
